b836a91f6a444db15a7929c220dd47086b5f3573
Jason Bowman <jasonb@unity3d.com>
2021-08-25 19:42:38 -0700

    Extend TrainingAnalytics side channel to expose configuration details
    Update python files to adhere to linting rules
    
    Fix typing check
    
    Simplify variable usage

--- ml-agents/mlagents/training_analytics_side_channel.py
+++ ml-agents/mlagents/training_analytics_side_channel.py
@@ -1,5 +1,9 @@
+import copy
+import json
+import hmac
+import hashlib
 import sys
-from typing import Optional
+from typing import Optional, Dict
 import uuid
 import mlagents_envs
 import mlagents.trainers
@@ -21,11 +25,21 @@ class TrainingAnalyticsSideChannel(SideChannel):
     Side channel that sends information about the training to the Unity environment so it can be logged.
     """
 
+    __vendorKey: str = "unity.ml-agents"
+
     def __init__(self) -> None:
         # >>> uuid.uuid5(uuid.NAMESPACE_URL, "com.unity.ml-agents/TrainingAnalyticsSideChannel")
         # UUID('b664a4a9-d86f-5a5f-95cb-e8353a7e8356')
         super().__init__(uuid.UUID("b664a4a9-d86f-5a5f-95cb-e8353a7e8356"))
         self.run_options: Optional[RunOptions] = None
+
+    @staticmethod
+    def __hash(key: str, data: str) -> str:
+        res = hmac.new(
+            key.encode("utf-8"), data.encode("utf-8"), hashlib.sha256
+        ).hexdigest()
+        print(res)
+        return res
 
     def on_message_received(self, msg: IncomingMessage) -> None:
         raise UnityCommunicationException(
@@ -33,11 +47,70 @@ class TrainingAnalyticsSideChannel(SideChannel):
             + "this should not have happened."
         )
 
+    @staticmethod
+    def __sanitize_run_options(config: RunOptions) -> Dict[str, Any]:
+        res = copy.deepcopy(config.as_dict())
+
+        def hash(value: str) -> str:
+            return TrainingAnalyticsSideChannel.__hash(
+                TrainingAnalyticsSideChannel.__vendorKey, value
+            )
+
+        # Filter potentially PII behavior names
+        if "behaviors" in res and res["behaviors"]:
+            res["behaviors"] = {hash(k): v for (k, v) in res["behaviors"].items()}
+            for (k, v) in res["behaviors"].items():
+                if "init_path" in v and v["init_path"] is not None:
+                    hashed_path = hash(v["init_path"])
+                    res["behaviors"][k]["init_path"] = hashed_path
+
+        # Filter potentially PII curriculum and behavior names from Checkpoint Settings
+        if "environment_parameters" in res and res["environment_parameters"]:
+            res["environment_parameters"] = {
+                hash(k): v for (k, v) in res["environment_parameters"].items()
+            }
+            for (curriculumName, curriculum) in res["environment_parameters"].items():
+                updated_lessons = []
+                for lesson in curriculum["curriculum"]:
+                    new_lesson = copy.deepcopy(lesson)
+                    if lesson.has_keys("name"):
+                        new_lesson["name"] = hash(lesson["name"])
+                    if lesson.has_keys("completion_criteria"):
+                        new_lesson["completion_criteria"]["behavior"] = hash(
+                            new_lesson["completion_criteria"]["behavior"]
+                        )
+                    updated_lessons.append(new_lesson)
+                res["environment_parameters"][curriculumName][
+                    "curriculum"
+                ] = updated_lessons
+
+        # Filter potentially PII filenames from Checkpoint Settings
+        if "checkpoint_settings" in res and res["checkpoint_settings"] is not None:
+            if (
+                "initialize_from" in res["checkpoint_settings"]
+                and res["checkpoint_settings"]["initialize_from"] is not None
+            ):
+                res["checkpoint_settings"]["initialize_from"] = hash(
+                    res["checkpoint_settings"]["initialize_from"]
+                )
+            if (
+                "results_dir" in res["checkpoint_settings"]
+                and res["checkpoint_settings"]["results_dir"] is not None
+            ):
+                res["checkpoint_settings"]["results_dir"] = hash(
+                    res["checkpoint_settings"]["results_dir"]
+                )
+
+        return res
+
     def environment_initialized(self, run_options: RunOptions) -> None:
         self.run_options = run_options
         # Tuple of (major, minor, patch)
         vi = sys.version_info
         env_params = run_options.environment_parameters
+        sanitized_run_options = TrainingAnalyticsSideChannel.__sanitize_run_options(
+            run_options
+        )
 
         msg = TrainingEnvironmentInitialized(
             python_version=f"{vi[0]}.{vi[1]}.{vi[2]}",
@@ -47,7 +120,10 @@ class TrainingAnalyticsSideChannel(SideChannel):
             torch_device_type=torch_utils.default_device().type,
             num_envs=run_options.env_settings.num_envs,
             num_environment_parameters=len(env_params) if env_params else 0,
+            run_options=json.dumps(sanitized_run_options),
         )
+
+        print(msg)
 
         any_message = Any()
         any_message.Pack(msg)
@@ -56,9 +132,22 @@ class TrainingAnalyticsSideChannel(SideChannel):
         env_init_msg.set_raw_bytes(any_message.SerializeToString())
         super().queue_message_to_send(env_init_msg)
 
+    @staticmethod
+    def __sanitize_trainer_settings(config: TrainerSettings) -> Dict[str, Any]:
+        config_dict = copy.deepcopy(config.as_dict())
+        if "init_path" in config_dict and config_dict["init_path"] is not None:
+            hashed_path = TrainingAnalyticsSideChannel.__hash(
+                TrainingAnalyticsSideChannel.__vendorKey, config_dict["init_path"]
+            )
+            config_dict["init_path"] = hashed_path
+        return config_dict
+
     def training_started(self, behavior_name: str, config: TrainerSettings) -> None:
+        raw_config = TrainingAnalyticsSideChannel.__sanitize_trainer_settings(config)
         msg = TrainingBehaviorInitialized(
-            behavior_name=behavior_name,
+            behavior_name=TrainingAnalyticsSideChannel.__hash(
+                self.__vendorKey, behavior_name
+            ),
             trainer_type=config.trainer_type.value,
             extrinsic_reward_enabled=(
                 RewardSignalType.EXTRINSIC in config.reward_signals
@@ -76,6 +165,7 @@ class TrainingAnalyticsSideChannel(SideChannel):
             trainer_threaded=config.threaded,
             self_play_enabled=config.self_play is not None,
             curriculum_enabled=self._behavior_uses_curriculum(behavior_name),
+            config=json.dumps(raw_config),
         )
 
         any_message = Any()
